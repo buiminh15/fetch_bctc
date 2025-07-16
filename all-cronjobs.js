ed6b5af9af9bc32af0fe8dedd7c51ec2f3457507
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-07-16 20:21:01 +0700

    fix: run cronjobs

--- all-cronjobs.js
+++ all-cronjobs.js
@@ -2,14 +2,14 @@
 require('./bctc-cdn');
 require('./bctc-khs');
 require('./bctc-sgn');
-require('./bctc-abt');
+// require('./bctc-abt');
 require('./bctc-acb');
 require('./bctc-ast');
 require('./bctc-bsr');
 require('./bctc-dcm');
 require('./bctc-dgc');
 require('./bctc-dhc');
-require('./bctc-dnw');
+// require('./bctc-dnw');
 require('./bctc-dri');
 
 // Thêm các file cronjob khác nếu có
