45100329a844f7ab187a86eeacf7eca17994b4ba
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-07-17 20:20:08 +0700

    fix: bctc-khs

--- all-cronjobs.js
+++ all-cronjobs.js
@@ -1,11 +1,8 @@
-
-require('./bctc-cdn');
-require('./bctc-khs');
-require('./bctc-sgn');
 // require('./bctc-abt');
 require('./bctc-acb');
 require('./bctc-ast');
 require('./bctc-bsr');
+require('./bctc-cdn');
 require('./bctc-dcm');
 require('./bctc-dgc');
 require('./bctc-dhc');
@@ -15,11 +12,14 @@ require('./bctc-geg');
 require('./bctc-ghc');
 require('./bctc-hhp');
 require('./bctc-hpg');
+require('./bctc-khs');
+require('./bctc-ncs');
 require('./bctc-nnc');
-require('./bctc-pat');
 require('./bctc-ntc');
+require('./bctc-pat');
 require('./bctc-pph');
 require('./bctc-pvp');
+require('./bctc-sgn');
 
 // Thêm các file cronjob khác nếu có
 
