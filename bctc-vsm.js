baa55e8cd73bf2f3f6c5de1ab6df8d491e772ee6
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-07-22 17:43:57 +0700

    feat: add bctc

---
+++ bctc-vsm.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const { sendTelegramNotification } = require('./bot');
+const { COMPANIES } = require('./constants/companies');
+const { insertBCTC, filterNewNames } = require('./bctc');
+console.log('📢 [bctc-geg.js:5]', 'running');
+
+const axiosRetry = require('axios-retry');
+
+axiosRetry.default(axios, {
+  retries: 3,
+  retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: (error) => {
+    // Retry nếu là network error, request idempotent, hoặc timeout
+    return axiosRetry.isNetworkOrIdempotentRequestError(error) || error.code === 'ECONNABORTED';
+  }
+});
+
+async function fetchAndExtractData() {
+  try {
+    const response = await axios.get(
+      'https://www.viconshipdanang.com/news/index-v3-json?widget_id=6117720&limit=12&_=1753179283341',
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Accept-Language': 'en-US,en;q=0.7',
+          'Cache-Control': 'no-cache',
+          'Connection': 'keep-alive',
+          'Origin': 'https://geccom.vn',
+          'Pragma': 'no-cache',
+          'Referer': 'https://geccom.vn/',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-site',
+          'Sec-GPC': '1',
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+          'X-Requested-Store': 'default',
+          'X-Requested-With': 'XMLHttpRequest',
+          'sec-ch-ua': '"Not)A;Brand";v="8", "Chromium";v="138", "Brave";v="138"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"'
+        },
+        timeout: 60000
+      }
+    );
+
+    // response.data là object JSON, thường có dạng { data: [ ... ], ... }
+    const items = response.data.items || [];
+    const names = items.map(item => item.name && item.name.trim()).filter(Boolean);
+
+    if (names.length === 0) {
+      console.log('Không tìm thấy báo cáo tài chính nào.');
+      return;
+    }
+
+    // Lọc ra các báo cáo chưa có trong DB
+    const newNames = await filterNewNames(names, COMPANIES.VSM);
+    console.log('📢 [bctc-geg.js:44]', newNames);
+    if (newNames.length) {
+      await insertBCTC(newNames, COMPANIES.VSM);
+
+      // Gửi thông báo Telegram cho từng báo cáo mới
+      await Promise.all(
+        newNames.map(name =>
+          sendTelegramNotification(`Báo cáo tài chính của VSM::: ${name}`)
+        )
+      );
+      console.log(`Đã thêm ${newNames.length} báo cáo mới và gửi thông báo.`);
+    } else {
+      console.log('Không có báo cáo mới.');
+    }
+  } catch (error) {
+    console.error('Error fetching API:', error.message);
+    process.exit(1);
+  }
+}
+
+fetchAndExtractData();
