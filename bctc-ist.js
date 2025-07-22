5c92ba8aa26ffe0fbfc20605aeaccc4e5abf1cd0
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2025-07-22 13:58:12 +0700

    feat: add bctc

--- bctc-ist.js
+++ bctc-ist.js
@@ -3,10 +3,9 @@ const cheerio = require('cheerio');
 const { sendTelegramNotification } = require('./bot');
 const { COMPANIES } = require('./constants/companies');
 const { insertBCTC, filterNewNames } = require('./bctc');
-const https = require('https');
-const agent = new https.Agent({
-  rejectUnauthorized: false
-});
+const he = require('he');
+console.log('📢 [bctc-cdn.js:7]', 'running');
+
 const axiosRetry = require('axios-retry');
 
 axiosRetry.default(axios, {
@@ -20,42 +19,43 @@ axiosRetry.default(axios, {
 
 async function fetchAndExtractData() {
   try {
-    const response = await axios.get('https://scsc.vn/vn/info_category.aspx?IDCAT=36', {
+    const response = await axios.get('https://www.icdsongthan.com.vn/bao-cao-tai-chinh', {
       headers: {
         'accept': 'text/html',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
       },
-      timeout: 60000,
-      httpsAgent: agent
+      timeout: 60000
     });
 
     const html = response.data;
     const $ = cheerio.load(html);
-
+    const currentYear = new Date().getFullYear().toString();
     // Lấy tối đa 5 báo cáo mới nhất
     const names = [];
-    $('a[href*="info_category_detail"]').each((_, element) => {
-      const name = $(element).text().trim();
-      // Ghép date và name để tăng tính duy nhất, hoặc custom lại nếu bạn muốn
-      names.push(name);
+    $('.blog-grid h6 a').each((_, el) => {
+      const nameRaw = $(el).text().trim();
+      const name = he.decode(nameRaw);
+      const filterCondition = [currentYear, 'báo cáo tài chính'];
+      if (filterCondition.every(y => name.toLocaleLowerCase().includes(y))) {
+        names.push(name);
+      }
     });
 
     if (names.length === 0) {
       console.log('Không tìm thấy báo cáo tài chính nào.');
       return;
     }
-    console.log('📢 [bctc-hhp.js:47]', names);
+    console.log('📢 [bctc-mbs.js:50]', names);
     // Lọc ra các báo cáo chưa có trong DB
-    const newNames = await filterNewNames(names, COMPANIES.SCS);
-    console.log('📢 [bctc-hhp.js:37]', newNames);
+    const newNames = await filterNewNames(names, COMPANIES.IST);
+    console.log('📢 [bctc-cdn.js:46]', newNames);
     if (newNames.length) {
-      await insertBCTC(newNames, COMPANIES.SCS);
+      await insertBCTC(newNames, COMPANIES.IST);
 
       // Gửi thông báo Telegram cho từng báo cáo mới
       await Promise.all(
         newNames.map(name => {
-
-          return sendTelegramNotification(`Báo cáo tài chính của SCS::: ${name}`);
+          return sendTelegramNotification(`Báo cáo tài chính của IST ::: ${name}`);
         })
       );
       console.log(`Đã thêm ${newNames.length} báo cáo mới và gửi thông báo.`);
@@ -67,5 +67,5 @@ async function fetchAndExtractData() {
     process.exit(1);
   }
 }
-console.log('📢 [bctc-hhp.js:56]', 'running');
+
 fetchAndExtractData();
