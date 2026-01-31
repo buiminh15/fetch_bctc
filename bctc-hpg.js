3dce0d7b7313df7acb26e85fd33b64981e209668
buiminh15 <40208756+buiminh15@users.noreply.github.com>
2026-01-31 14:33:25 +0700

    fix: hpg

--- bctc-hpg.js
+++ bctc-hpg.js
@@ -30,12 +30,17 @@ async function fetchAndExtractData() {
 
     const names = [];
 
-    $('.name a').each((index, el) => {
-      if (index < 3) {
-        const name = $(el).text().trim();
-        names.push(name);
+    $('a').each((i, el) => {
+      const text = $(el)
+        .text()
+        .replace(/\s+/g, ' ')
+        .trim();
+
+      if (text.startsWith('Báo cáo tài chính')) {
+        names.push(text);
       }
     });
+
 
     if (names.length === 0) {
       console.log('Không tìm thấy báo cáo tài chính nào.');
@@ -65,3 +70,57 @@ async function fetchAndExtractData() {
 }
 console.log('📢 [bctc-hpg.js:53]', 'running');
 fetchAndExtractData();
+
+async function fetchBCSXKD() {
+  try {
+    const response = await axios.get('https://www.hoaphat.com.vn/quan-he-co-dong/cong-bo-thong-tin?page=1', {
+      headers: {
+        'accept': 'text/html',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36',
+      },
+      timeout: 60000
+    });
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const names = [];
+
+    $('.name a').each((i, el) => {
+      const text = $(el)
+        .text()
+        .replace(/\s+/g, ' ')
+        .trim();
+
+      names.push(text);
+    });
+
+
+    if (names.length === 0) {
+      console.log('Không tìm thấy báo cáo tài chính nào.');
+      return;
+    }
+
+    // Lọc ra các báo cáo chưa có trong DB
+    const newNames = await filterNewNames(names, COMPANIES.HPG);
+    console.log('📢 [bctc-hpg.js:35]', newNames);
+    if (newNames.length) {
+      await insertBCTC(newNames, COMPANIES.HPG);
+
+      // Gửi thông báo Telegram cho từng báo cáo mới
+      // await Promise.all(
+      //   newNames.map(name => {
+      //     return sendTelegramNotification(`Công bố thông tin của Hòa Phát::: ${name}`);
+      //   })
+      // );
+      console.log(`Đã thêm ${newNames.length} báo cáo mới và gửi thông báo.`);
+    } else {
+      console.log('Không có báo cáo mới.');
+    }
+  } catch (error) {
+    console.error('Error fetching HTML:', error);
+    process.exit(1);
+  }
+}
+
+fetchBCSXKD();
